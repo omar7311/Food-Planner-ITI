0d9600d09922735f065ccfd0c632c053c78596f7
Omar Gohary <omar.gohary123@gmail.com>
2024-08-11 23:30:38 +0200

    add nessessory library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.food_planner_iti"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.food_planner_iti"
@@ -32,6 +32,33 @@ android {
 }
 
 dependencies {
+    //room
+    implementation ("androidx.room:room-runtime:2.6.1")
+    annotationProcessor ("androidx.room:room-compiler:2.6.1")
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    // Gson
+    implementation ("com.google.code.gson:gson:2.10.1")
+    // Retrofit Gson Converter
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    // circle imageview
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    // sdp
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+    //ssp
+    implementation( "com.intuit.ssp:ssp-android:1.1.0")
+    //firebase
+    implementation("com.firebaseui:firebase-ui-storage:7.2.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-database")
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
+    //lottie
+    implementation ("com.airbnb.android:lottie:6.2.0")
 
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
