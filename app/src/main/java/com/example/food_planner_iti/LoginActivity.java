ffadb7a545afab6bf9a1cbfa2432e813b4d1c0cd
Omar Gohary <omar.gohary123@gmail.com>
2024-08-13 00:39:59 +0200

    add splash screen

---
+++ app/src/main/java/com/example/food_planner_iti/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.food_planner_iti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
